18f9e14dea04f444c8b1a94a70f5089c47768556
Sudheer Ahmed <sudheerahmed.kcsf18@iba-suk.edu.pk>
2022-05-24 17:12:06 +0500

    completed

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const mongoose = require('mongoose')
-const Article = require('./models/article')
+const User = require('./models/user')
 const app = express()
-const articleRouter = require('./routes/articles')
+const userRouter = require('./routes/users')
 const methodOverride = require('method-override')
 
-mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/userDB',{ useNewUrlParser: true, useUnifiedTopology: true})
 
 app.set('view engine', 'ejs')
 
@@ -13,10 +13,10 @@ app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
 
 app.get('/',async (req,res) => {
-    const articles = await Article.find().sort({createdAt: 'desc'})
+    const users = await User.find()
 
-   res.render('articles/index', { articles:articles })
+   res.render('users/index', { users:users })
 })
-app.use('/articles',articleRouter)
+app.use('/users',userRouter)
 
 app.listen(5000)
